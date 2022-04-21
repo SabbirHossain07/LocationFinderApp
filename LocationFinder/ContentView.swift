8ed59fc4add789b325095b00813cc37170eae0fe
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-04-22 00:21:48 +0600

    Initial Commit

---
+++ LocationFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocationFinder
+//
+//  Created by Sopnil Sohan on 22/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocationFinder/LocationFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationFinderApp.swift
+//  LocationFinder
+//
+//  Created by Sopnil Sohan on 22/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LocationFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
